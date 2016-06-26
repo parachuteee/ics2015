636c0184f7a2fa6995a9e6e24bb8bc62caf78fb0
tracer-ics2015 <tracer@njuics.org>
2016-06-27 04:30:54 +0800

    >  compile NEMU 13307130513 parachute Linux ubuntu 3.16.0-46-generic #62~14.04.1-Ubuntu SMP Tue Aug 11 16:27:16 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  04:30:54 up  6:44,  2 users,  load average: 1.41, 1.46, 1.23 4546d8b7443243dac2d4cd0c88c450329d3de70c

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -6,25 +6,44 @@
 #include <sys/types.h>
 #include <regex.h>
 
+bool flag = 1;
+int Num_type = 10;
+
 enum {
-	NOTYPE = 256, EQ
-
-	/* TODO: Add more token types */
-
+	NOTYPE = 256, DIGIT = 0x30, 
+	HEX, EQ, NEQ,
+	AND, OR, NOT,
+	REG_NAME, REG, OBJ
 };
+
+extern uint32_t get_addr_obj(char*);
 
 static struct rule {
 	char *regex;
 	int token_type;
 } rules[] = {
 
-	/* TODO: Add more rules.
+	/* Add more rules.
 	 * Pay attention to the precedence level of different rules.
 	 */
+	{" +",	NOTYPE},					// spaces
+	{"0x", HEX},						// hex
+	{"\\$", REG_NAME},					// reg name
+	{"[eE][abcdABCD][xX]|[eE][sbiSBI][pP]|[eE][sdSD][iI]|[ABCDabcd][xX]|[sbSB][pP]|[sdSD][iI]|[abcdABCD][lhLH]", REG},								//registers
+	{"[A-Za-z_][0-9A-Za-z_]+", OBJ},		// OBJ
+	{"[0-9A-Fa-f]+", DIGIT},			// digits
+	{"\\(", '('},						// left
+	{"\\)", ')'},						// right
+	{"\\*", '*'},						// multiple
+	{"\\/", '/'},						// divide
+	{"\\+", '+'},						// plus
+	{"\\-", '-'},						// minus
+	{"==", EQ},							// equal
+	{"!=", NEQ},						// not equal
+	{"[&]{2}", AND},					// and
+	{"[|]{2}", OR},						// or
+	{"\\!", NOT},						// not
 
-	{" +",	NOTYPE},				// spaces
-	{"\\+", '+'},					// plus
-	{"==", EQ}						// equal
 };
 
 #define NR_REGEX (sizeof(rules) / sizeof(rules[0]) )
@@ -34,6 +53,7 @@ static regex_t re[NR_REGEX];
 /* Rules are used for many times.
  * Therefore we compile them only once before any usage.
  */
+
 void init_regex() {
 	int i;
 	char error_msg[128];
@@ -56,29 +76,86 @@ typedef struct token {
 Token tokens[32];
 int nr_token;
 
+int check_brackets(int, int);
+
+int Judge_op(int, int);
+
+uint32_t eval(int, int);
+
 static bool make_token(char *e) {
 	int position = 0;
 	int i;
 	regmatch_t pmatch;
 	
-	nr_token = 0;
+	tokens[0].type = '(';
+	nr_token = 1;
 
 	while(e[position] != '\0') {
-		/* Try all rules one by one. */
-		for(i = 0; i < NR_REGEX; i ++) {
+		for(i = 0; i < NR_REGEX; ++i) {
 			if(regexec(&re[i], e + position, 1, &pmatch, 0) == 0 && pmatch.rm_so == 0) {
-				char *substr_start = e + position;
 				int substr_len = pmatch.rm_eo;
-
-				Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s", i, rules[i].regex, position, substr_len, substr_len, substr_start);
 				position += substr_len;
 
-				/* TODO: Now a new token is recognized with rules[i]. Add codes
+				/* Now a new token is recognized with rules[i]. Add codes
 				 * to record the token in the array ``tokens''. For certain 
 				 * types of tokens, some extra actions should be performed.
 				 */
 
 				switch(rules[i].token_type) {
+					int j;
+					case NOTYPE: break;
+					case OBJ:
+							position -= substr_len;
+							j = 0;
+							while (e[position] != '\0' && ((e[position] >= 'a' && e[position] <= 'z') || (e[position] >= 'A' && e[position] <= 'Z') || (e[position] >= '0' && e[position] <= '9') || e[position] == '_')) {
+								tokens[nr_token].str[j ++] = e[(position ++)];
+							}
+							tokens[nr_token].str[j] = '\0';
+
+							if (get_addr_obj(tokens[nr_token].str)) {
+								tokens[nr_token].type = OBJ;
+								++ nr_token;
+							}
+							else {
+								printf("object not exist!\n");
+							} 
+							break;					
+					case DIGIT:
+							position -= substr_len;
+							tokens[nr_token].type = DIGIT;
+							j = 0;
+							while (e[position] != '\0' && j < 32 && ((e[position] <= '9' && e[position] >= '0') || (e[position] <= 'Z' && e[position] >= 'A') || (e[position] >= 'a' && e[position] <= 'z'))) 
+								tokens[nr_token].str[j ++] = e[(position ++)];
+							tokens[nr_token].str[j] = '\0';
+							if (j >= 32 && !((e[position] <= '9' && e[position] >= '0') || (e[position] <= 'Z' && e[position] >= 'A') || (e[position] >= 'a' && e[position] <= 'z'))) {
+								j ++;
+								assert(0);
+							}
+							++ nr_token; 
+							break; 
+					case HEX: tokens[nr_token ++].type = HEX; break;
+					case EQ: tokens[nr_token ++].type = EQ; break;
+					case NEQ: tokens[nr_token ++].type = NEQ; break;
+					case '(': tokens[nr_token ++].type = '('; break;
+					case ')': tokens[nr_token ++].type = ')'; break;
+					case '*': tokens[nr_token ++].type = '*'; break;
+					case '/': tokens[nr_token ++].type = '/'; break;
+					case '+': tokens[nr_token ++].type = '+'; break;
+					case '-': tokens[nr_token ++].type = '-'; break;
+					case AND: tokens[nr_token ++].type = AND; break;
+					case OR: tokens[nr_token ++].type = OR; break;
+					case NOT: tokens[nr_token ++].type = NOT; break;
+					case REG_NAME: tokens[nr_token ++].type = REG_NAME; break; 
+					case REG:
+							position -= substr_len;
+							tokens[nr_token].type = REG;
+							j = 0;
+							while (j < 32 && ((e[position]>='A' && e[position]<='Z') || (e[position]>='a' && e[position]<='z'))) 
+								tokens[nr_token].str[j ++] = e[(position ++)];
+							tokens[nr_token].str[j] = '\0';
+							++nr_token; 
+							break; 
+
 					default: panic("please implement me");
 				}
 
@@ -91,18 +168,279 @@ static bool make_token(char *e) {
 			return false;
 		}
 	}
-
-	return true; 
+	tokens[nr_token ++].type = ')';
+	return true;
 }
 
-uint32_t expr(char *e, bool *success) {
+uint32_t expr(char *e, bool *flag) {
 	if(!make_token(e)) {
-		*success = false;
+		*flag = false;
 		return 0;
 	}
+	/* Insert codes to evaluate the expression. */
+	*flag = true;
+	return eval(0, nr_token - 1);
+}
 
-	/* TODO: Insert codes to evaluate the expression. */
-	panic("please implement me");
+
+
+int check_brackets(int s, int e) {
+/* check if there are illegal brackets,
+ * such like ")(", or "()", or "(...))", or "((...)"
+ */
+	int i, left_bra = 0, flag_in = 0, flag_out = 1;
+	for (i = s; i < e + 1; ++ i) {
+		if (left_bra < 0) return -1;
+		if (flag_in == 1 && left_bra == 0) flag_out = 0;
+		if (tokens[i].type == '(') {
+			if (tokens[i - 1].type == ')') return -1; 
+			else { ++ left_bra; flag_in = 1; }
+		}
+		if (tokens[i].type == ')') {
+			if (tokens[i - 1].type == '(') return -1;
+			else { -- left_bra; flag_in = 1; }
+		}
+	}
+	if (left_bra == 0) {
+		if (tokens[s].type == '(' && tokens[e].type == ')' && flag_out == 1) 
+			return 1;
+		else return 0;
+	}
+	else return -1;
+}
+
+int Judge_op(int s, int e) {
+/* Priority :
+ * *, /
+ * +, -
+ * ==, !=
+ * &&
+ * ||
+ */
+	int i = e;		// Judge "||"
+	while (i > s - 1) { 
+		if (tokens[i].type == ')') {
+			int j = i - 1; 
+			while (check_brackets(j, i) != 1) 
+				-- j;
+			i = j;
+		}
+		-- i;
+		if (i <= s)
+			break;
+		if (tokens[i].type == OR) 
+			return i;
+	}
+	i = e;			// Judge "&&"
+	while (i > s - 1) {
+		if (tokens[i].type == ')') {
+			int j = i - 1; 
+			while (check_brackets(j, i) != 1) 
+				-- j;
+			i = j;
+		}
+		-- i;
+		if (i <= s)
+			break;
+		if (tokens[i].type == AND) 
+			return i;
+	}
+	i = e;			// Judge "=="
+	while (i > s - 1) {
+		if (tokens[i].type == ')') {
+			int j = i - 1; 
+			while (check_brackets(j, i) != 1) 
+				-- j;
+			i = j;
+		}
+		-- i;
+		if (i <= s)
+			break;
+		if (tokens[i].type == EQ) 
+			return i;
+ 	}
+ 	i = e;			// Judge "!="
+ 	while (i > s - 1) {
+		if (tokens[i].type == ')') {
+			int j = i - 1; 
+			while (check_brackets(j, i) != 1) 
+				-- j;
+			i = j;
+ 		}
+		-- i;
+		if (i <= s)
+			break;
+		if (tokens[i].type == NEQ) 
+			return i;
+	}
+	i = e;			// Judge "+, -"
+	while (i > s - 1) {
+		if (tokens[i].type == ')') {
+			int j = i - 1; 
+			while (check_brackets(j, i) != 1) 
+				-- j;
+			i = j;
+		}
+		-- i;
+		if (i <= s)
+			break;
+		bool flag = (tokens[i - 1].type == DIGIT) || (tokens[i - 1].type == ')'); 
+		if (tokens[i].type == '+' || (tokens[i].type == '-' && flag)) 
+			return i;
+	}
+	i = e;			// Judge "*, /"
+	while (i > s - 1) {
+		if (tokens[i].type == ')') {
+			int j = i - 1; 
+			while (check_brackets(j, i) != 1) 
+				-- j;
+			i = j;
+		}
+		-- i;
+		if (i <= s)
+			break;
+		bool flag = (tokens[i - 1].type == DIGIT) || (tokens[i - 1].type == ')'); 
+		if (tokens[i].type == '/' || (tokens[i].type == '*' && flag))
+			return i;
+	}
+	return -1;
+}
+
+uint32_t regname(char str[]) {
+	if (!strcmp(str, "eax"))
+		return cpu.eax;
+	if (!strcmp(str, "ecx"))
+		return cpu.ecx;
+	if (!strcmp(str, "edx"))
+		return cpu.edx;
+	if (!strcmp(str, "ebx")) 
+		return cpu.ebx;
+	if (!strcmp(str, "esp"))
+		return cpu.esp;
+	if (!strcmp(str, "ebp"))
+		return cpu.ebp;
+	if (!strcmp(str, "esi"))
+		return cpu.esi;
+	if (!strcmp(str, "edi"))
+		return cpu.edi;
+	if (!strcmp(str, "eip"))
+		return cpu.eip;
 	return 0;
 }
 
+uint32_t eval(int start, int end) {
+	if(start > end) {
+		flag = 0;
+		return 0;
+	}
+	else if(start == end) { 
+		if (tokens[end].type != DIGIT) {
+			if (tokens[end].type == OBJ) {
+				return get_addr_obj(tokens[end].str);
+			}
+			else {
+				flag = 0; 
+				return 0;
+			}
+		}
+		else {
+			if (tokens[end].type == DIGIT && Num_type == 10) {
+				uint32_t num = 0;
+				int i, len = strlen(tokens[end].str);
+			
+				for (i = 0; i < len; ++ i) 
+					if (!(tokens[end].str[i] >= '0' && tokens[end].str[i] <= '9')) {
+						flag = 0;
+						return 0;
+					}
+					else {
+						num = num * 10 + tokens[end].str[i] - '0';
+					}
+			
+				return num;
+			}
+			else if (tokens[end].type == DIGIT && Num_type == 0x10) {
+				uint32_t num = 0;
+				int i, len = strlen(tokens[end].str);
+	
+				for (i = 0; i < len; ++ i) 
+					if (tokens[end].str[i] >= '0' && tokens[end].str[i] <= '9') 
+						num = (num << 4) + tokens[end].str[i] - '0';
+					else if (tokens[end].str[i] >= 'A' && tokens[end].str[i] <= 'Z')
+						num = (num << 4) + tokens[end].str[i] - 'A' + 10;
+					else if (tokens[end].str[i] >= 'a' && tokens[end].str[i] <= 'z')
+						num = (num << 4) + tokens[end].str[i] - 'a' + 10;
+					else {
+						flag = 0;
+						assert(0);
+					}
+	
+				Num_type = 10;
+				return num;
+			}
+		}
+	}	
+	else if (tokens[start].type == HEX) {
+		Num_type = 0x10;
+		return eval(start + 1, end);
+	}
+	else {
+		int check = check_brackets(start, end);
+	 	if (check == 1) {
+			return eval(start + 1, end - 1); 
+		}
+		else if (check == -1) 
+			assert(0);
+ 		else if (check == 0) {
+			int sign = 1;
+			int S = start;
+			while (tokens[S].type == '-') {
+				sign *= -1;
+				++ S;
+			} 
+
+			int op = Judge_op(start, end);
+			if (op == -1) {
+				if (tokens[start].type == REG_NAME) {
+					if (end - start == 1)
+						return regname(tokens[end].str);
+					{
+						printf("Wrong register!\n");
+						assert(0);
+					}
+				}
+				if (tokens[start].type == NOT)						// Judge '!'
+					return !eval(start + 1, end);
+				else if (tokens[start].type == '-')					// Judge minus number
+					return sign * eval(S, end);						// A minus number = sign * abs(number)
+				else if (tokens[start].type == '*')
+					return swaddr_read(eval(start + 1, end), 4);	// Judge pointer dereference sign
+			}
+ 
+			uint32_t val1 = sign * eval(start, op - 1);	
+			int op_minus = op;
+			/* Deal minus number here
+			 */
+			sign = 1;
+			while (tokens[op_minus + 1].type == '-') {
+				sign *= -1;
+				op_minus ++;
+			}
+			uint32_t val2 = sign * eval(op_minus + 1, end);
+			// A minus number = sign * abs(number)
+ 
+			switch(tokens[op].type) {
+				case '+': return val1 + val2; break;
+				case '-': return val1 - val2; break;
+				case '*': return val1 * val2; break;
+				case '/': return val1 / val2; break;
+				case OR: return val1 || val2; break;
+				case AND: return val1 && val2; break;
+				case EQ: return val1 == val2; break;
+				case NEQ: return val1 != val2; break;
+				default: assert(0);
+			}
+		}
+ 	}
+	return 0;
+}
--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -3,21 +3,83 @@
 
 #define NR_WP 32
 
-static WP wp_pool[NR_WP];
-static WP *head, *free_;
+static WP wp_list[NR_WP];
+WP *head, *free_;
 
-void init_wp_pool() {
+void init_wp_list() {
 	int i;
 	for(i = 0; i < NR_WP; i ++) {
-		wp_pool[i].NO = i;
-		wp_pool[i].next = &wp_pool[i + 1];
+		wp_list[i].NO = i; 
+		wp_list[i].next = &wp_list[i + 1];
+		wp_list[i].addr[0] = 0;
 	}
-	wp_pool[NR_WP - 1].next = NULL;
+	wp_list[NR_WP - 1].next = NULL;
 
 	head = NULL;
-	free_ = wp_pool;
+	free_ = wp_list;
 }
 
-/* TODO: Implement the functionality of watchpoint */
+WP *new_wp() {
+	WP *temp_f = free_;
 
+	if (temp_f == NULL)
+		assert(0);
+	WP *New = malloc(sizeof(WP));
+	New->NO = temp_f->NO;
+	New->next = temp_f->next;
+	New->old_value = temp_f->old_value;
 
+	if (head == NULL) {
+		New->next = NULL;
+		head = New;
+	}
+	else {
+		WP *temp = head;
+		while (temp->next != NULL) 
+			temp = temp->next;
+		New->next = NULL;
+		temp->next = New;
+	}
+	free_ = free_->next;
+	return New;	
+}
+
+void free_wp(WP *wp) {
+	if (wp == NULL) assert(0);
+
+	WP *temp_f = free_;
+	WP *New = malloc(sizeof(WP));
+	New->NO = wp->NO;
+	New->next = NULL;
+	New->addr[0] = 0;
+	New->old_value = 0;
+
+	if (temp_f == NULL) {
+		free_ = New;
+	}
+	else {
+		WP *last_f = free_;
+		while (temp_f->next != NULL && temp_f->NO < wp->NO) {
+			last_f = temp_f;
+			temp_f = temp_f->next;
+		}
+		if (temp_f == free_) {
+			New->next = free_;
+			free_ = New;
+		}
+		else {
+			last_f->next = New;
+			New->next = temp_f;
+		}
+	}
+
+	if (wp == head)
+		head = head->next;
+	else {
+		WP *temp_h = head;
+		while (temp_h->next != wp)
+			temp_h = temp_h->next;
+		temp_h->next = wp->next;
+	}
+
+}
